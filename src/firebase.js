f5943f894361cf0b79b906c796ff891a9210d9c0
hizliparagraf <elifaydemire@gmail.com>
2025-11-11 17:08:24 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
 
-// Firebase Config 
 const firebaseConfig = {
   apiKey: "AIzaSyBiltFmigYTwZdw1paw4sRfoZCpyp9BIoM",
   authDomain: "hizliparagrafatolyesi.firebaseapp.com",
@@ -12,10 +10,5 @@ const firebaseConfig = {
   appId: "1:151714928217:web:8d5a41e81c9576d463c348"
 
 };
-
-// Firebase'i başlat
 const app = initializeApp(firebaseConfig);
-
-// Authentication ve Firestore
 export const auth = getAuth(app);
-export const db = getFirestore(app);
