72af414ddf5732ea034f36f65b9684d9ed7aa0c6
hizliparagraf <elifaydemire@gmail.com>
2025-11-11 17:20:17 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   storageBucket: "hizliparagrafatolyesi.firebasestorage.app",
   messagingSenderId: "151714928217",
   appId: "1:151714928217:web:8d5a41e81c9576d463c348"
-
 };
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
