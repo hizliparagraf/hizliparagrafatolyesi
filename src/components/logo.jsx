6ba91784339cea3cc2e402490d107ec91c695524
hizliparagraf <elifaydemire@gmail.com>
2025-11-12 19:37:32 +0300

    Update logo.jsx

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -1,7 +1,1 @@
-export default function Logo() {
-  return (
-    <div className="flex items-center gap-2">
-      <span className="text-indigo-600 font-bold text-2xl">HızlıParagraf</span>
-    </div>
-  );
-}
+
